5296b6b93fd06f8b2cbe36ab57f70387f5b7dede
Matthew Murphy <matt@mattmurphy.ca>
2022-07-12 19:16:48 -0600

    set up tauri app, remove cruft

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+	<div
+		className='App'
+		style={{
+			height: '100%',
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center'
+		}}
+	>
+		<div
+			className='draggable'
+			data-tauri-drag-region
+			style={{
+				position: 'absolute',
+				height: '100%',
+				width: '100%'
+			}}
+		/>
+		<h1>Welcome</h1>
+	</div>
+)
 
-export default App;
+export default App
